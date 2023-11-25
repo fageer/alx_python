69873476e9739e5a0e78183788827a5ee8c68bc9
fageer <fageern@gmail.com>
2023-11-25 22:06:52 +0300

    m

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,5 @@
-for i in range(10):
+for i in range(1, 10):
     for j in range(i + 1, 10):
         if i != j:
-            print("{:01d}{:01d}".format(i, j), end=", ")
-print("\b\b \n")
+            print("{:01d}{:01d}, ".format(i, j), end="")
+print("\b\b\n")
