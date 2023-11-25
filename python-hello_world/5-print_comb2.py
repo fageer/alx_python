d70552804c91f4b6c453c11073d8f17162597da8
fageer <fageern@gmail.com>
2023-11-25 18:10:48 +0300

    m

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,5 @@
 for i in range(100):
-    print("{:02d}".format(i),end=", ")
-    if i == 99:
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else: 
         print("{:02d}\n".format(i))
-        
-    
