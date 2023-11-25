a79046c87cab9020868c6292d84fda7ad4f09776
fageer <fageern@gmail.com>
2023-11-25 21:53:26 +0300

    m

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,3 @@
 for i in range(1, 10):
-    for j in range(i, 10):
-        if i < 9:
-            print("{:02d}, ".format(j), end="")
-        elif i == 9 and j == 9:
-            print("{:02d}".format(j))
+    for j in range(i + 1, 10):
+        print("{:02d}, {:02d}".format(i, j), end=", " if i < 9 or j < 9 else "\n")
