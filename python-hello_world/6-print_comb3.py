175c35bbbafc567b8ba035217b0ab8c555e7dc80
fageer <fageern@gmail.com>
2023-11-26 07:18:11 +0300

    m

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,11 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{:01d}{:01d}".format(i, j), end="")
-        else:
-            print("{:01d}{:01d}".format(i, j), end=", ")
+        print("{:01d}{:01d}".format(i, j), end=", " if not (i == 8 and j == 9) else "")
 print("\n")
+# for i in range(10):
+#     for j in range(i + 1, 10):
+#         if i == 8 and j == 9:
+#             print("{:01d}{:01d}".format(i, j), end="")
+#         else:
+#             print("{:01d}{:01d}".format(i, j), end=", ")
+# print("\n")
