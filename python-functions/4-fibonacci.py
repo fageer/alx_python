e7389486ccc52fdd589ba5cd9ee402d62bd8f7e7
fageer <fageern@gmail.com>
2023-11-27 00:00:34 +0300

    m

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,11 +1,6 @@
 def fibonacci_sequence(n):
-    if n == 0:
-        return [] 
-    elif n == 1:
-        return [0]
-    elif n == 2:
-        return [0, 1]
     fibunacci = [0, 1]
-    for i in range(2, n):
-        fibunacci = fibunacci + [fibunacci[-1] + fibunacci[-2]]
-    return fibunacci
+    while len(fibunacci) < n:
+            anothernum = fibunacci[-1] + fibunacci[-2]
+            fibunacci = fibunacci + [anothernum]
+    return fibunacci[:n]
