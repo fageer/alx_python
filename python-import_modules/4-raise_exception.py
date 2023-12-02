d269893dedb746b911cc02da1e5536455681adb2
fageer <fageern@gmail.com>
2023-12-02 13:42:02 +0300

    m

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,5 +1,2 @@
 def raise_exception():
-    try:
-        raise_exception()
-    except TypeError as te:
-        print("Exception has been raised")
+    raise TypeError("This is a deliberate type exception.")
