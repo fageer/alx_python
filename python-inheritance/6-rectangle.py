a80a895d9dc90b82f957a7a80d2e59f7aa07fd86
fageer <fageern@gmail.com>
2023-12-11 23:10:16 +0300

    m

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,5 +1,7 @@
 """Import '5-base_geometry' File"""
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Define a'Rectangle' Class"""
     def __init__(self, width, height):
