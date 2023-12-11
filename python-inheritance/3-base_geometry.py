7e3110f7c4523927c680462379c7d3bef036cb93
fageer <fageern@gmail.com>
2023-12-11 13:56:59 +0300

    m

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,10 +1,5 @@
 """Define an Empty Class"""
 class BaseGeometry:
     """it's an empty class"""
-    def __dir__(cls):
-        """Removing __init_subclass__ attribute
-        from the dir result to pass the check
-        """
-        return [attr for attr in super().__dir__() if
-                attr != '__init_subclass__']
+    pass
     
