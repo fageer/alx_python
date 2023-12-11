4b8f589e2b6f43c31f2d345a26bcb0d4be401bd9
fageer <fageern@gmail.com>
2023-12-11 13:07:24 +0300

    m

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,4 +1,4 @@
-"""Define function that check the object 
-is exactly an instance of the specified class """#
 def is_same_class(obj, a_class):
+    """Define function that check the object 
+    is exactly an instance of the specified class """
     return True if isinstance(obj,a_class) else False 
