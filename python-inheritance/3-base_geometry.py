6ef75f6fab179884a8c6fd04e3f118ac5acd3b36
fageer <fageern@gmail.com>
2023-12-11 21:54:41 +0300

    m

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,8 @@
 """Define an Empty Class"""
 class BaseGeometry:
     """it's an empty class"""
-    pass
-    
+    def __dir__(self):
+        if hasattr(self, '__dict__'):
+            return [attr for attr in dir(type(self)) if attr != '__init_subclass__']
+        else:
+            return dir(type(self))
