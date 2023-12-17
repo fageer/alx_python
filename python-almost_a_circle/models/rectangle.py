165b85db3472fa98f3eff2150068b4a51b85c7e0
fageer <fageern@gmail.com>
2023-12-17 18:57:58 +0300

    m

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,9 @@ from base import Base
 class Rectangle(Base):
     """Define The __init__ method"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """inherit the init method from the super class"""
         super().__init__(self, id)
+        """Assign each argument """
         self.__width = width
         self.__height = height
         self.__x = x
