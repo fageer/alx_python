babfdb9264e48bb171fff0b32b682d2cf8d35718
fageer <fageern@gmail.com>
2023-12-17 19:51:30 +0300

    m

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,11 +3,10 @@ from models.base import Base
 
 """Define the Rectangle class"""
 class Rectangle(Base):
-    """Define The __init__ method"""
+    """Class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """inherit the init method from the super class"""
-        super().__init__(self, id)
-        """Assign each argument """
+        """Creates instance object"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
@@ -16,47 +15,47 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Getter method for width"""
-        return self.width
+        """Getter method for Width"""
+        return self.__width
     
     
-    @width.setattr
+    @width.setter
     def width(self, value):
         """Setter method for width"""
-        self.width = value
+        self.__width = value
 
     
     @property
     def height(self):
         """Getter method for height"""
-        return self.height
+        return self.__height
     
     
-    @height.setattr
+    @height.setter
     def height(self, value):
         """Setter method for height"""
-        self.height = value
+        self.__height = value
 
 
     @property
     def x(self):
         """Getter method for x"""
-        return self.x
+        return self.__x
         
         
-    @x.setattr
+    @x.setter
     def x(self, value):
         """Setter method for x"""
-        self.x = value
+        self.__x = value
 
     
     @property
     def y(self):
         """Getter method for y"""
-        return self.y
+        return self.__y
         
         
-    @y.setattr
+    @y.setter
     def y(self, value):
         """Setter method for y"""
-        self.y = value
+        self.__y = value
