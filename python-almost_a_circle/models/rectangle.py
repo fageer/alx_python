2b2a5cfd042ef4a2d518444bc57212a616f5e66f
fageer <fageern@gmail.com>
2023-12-17 20:31:13 +0300

    6. Display #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,7 +17,7 @@ class Rectangle(Base):
         """STR method"""
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"#
     
-    
+
     @property
     def width(self):
         """Getter method for Width"""
