8121f279321da1ae7ce9126eab42e3f118d41117
fageer <fageern@gmail.com>
2023-12-18 20:42:21 +0300

    m

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,5 +5,6 @@ import requests
 if __name__ == "__main__":
     """get the body of the response"""
     response = requests.get("https://alu-intranet.hbtn.io/status")
+    print("Body response:")
     print(f"- type: {type(response.text)}")
     print(f"- content: {response.text}")
