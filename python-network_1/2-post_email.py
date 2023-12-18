fce80d45d89b317b53b5bb0f65688169a5159827
fageer <fageern@gmail.com>
2023-12-18 21:11:43 +0300

    2. POST an email #1

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     response = requests.post(url, data=email)
-    print(f"Your email is: {email}")
+    print(f"Email: {email}")
