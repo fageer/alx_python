0ef14c333642d2779f7ae5b9bc701c7527a8681c
fageer <fageern@gmail.com>
2023-12-18 23:17:42 +0300

    5. My GitHub!

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -6,9 +6,9 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    auth = {'Authorization': f'Bearer {password}'}
-    response = requests.get("https://api.github.com/user", auth=auth)
+    headers = {'Authorization': f'Bearer {password}'}
+    response = requests.get("https://api.github.com/user", headers=headers)
     if response.status_code >= 400:
         print("None")
-
-    print(response.json()["id"])
+    else:
+        print(response.json()["id"])
