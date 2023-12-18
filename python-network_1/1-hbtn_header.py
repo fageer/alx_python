c8baa4ba5cc1794c8f242424a1386dd2d4bc59c1
fageer <fageern@gmail.com>
2023-12-18 21:01:40 +0300

    1. Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,5 +6,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    value = response.headers.get(['X-Request-Id'])
+    value = response.headers.get('X-Request-Id')
     print(value)
