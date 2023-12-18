629a2f994077b241d94ba6234c055f9aad414a08
fageer <fageern@gmail.com>
2023-12-18 23:26:30 +0300

    5. My GitHub!

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,14 +1,12 @@
 """importing the requests and sys packages"""
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    headers = {'Authorization': f'Bearer {password}'}
-    response = requests.get("https://api.github.com/user", headers=headers)
-    if response.status_code >= 400:
-        print("None")
-    else:
-        print(response.json()["id"])
+    response = requests.get("https://api.github.com/user", auth=HTTPBasicAuth(username, password))
+    data = response.json()
+    print(data.get("id"))
