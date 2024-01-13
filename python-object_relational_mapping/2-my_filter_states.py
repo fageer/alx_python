d23c2b0c9324461462dae95f2b60b74b57d498da
fageer <fageern@gmail.com>
2024-01-13 14:33:35 +0300

    2. Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ conn = MySQLdb.connect(host='localhost',
 cur = conn.cursor()
 
 
-cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+cur.execute("SELECT * FROM states WHERE name = LOWER('{}') ORDER BY id ASC".format(argv[4]))
 
 records = cur.fetchall()
 
