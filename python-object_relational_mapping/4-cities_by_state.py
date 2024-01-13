154120ad631ea9362404c5649efffe301b70e2d9
fageer <fageern@gmail.com>
2024-01-13 14:51:19 +0300

    4. Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+import MySQLdb
+from sys import argv
+
+
+conn = MySQLdb.connect(host='localhost', 
+                       port=3306, 
+                       user=argv[1], 
+                       passwd=argv[2], 
+                       database=argv[3])
+
+cur = conn.cursor()
+
+
+cur.execute("SELECT * FROM cities ORDER BY id ASC")
+
+records = cur.fetchall()
+
+for record in records:
+    print(record)
