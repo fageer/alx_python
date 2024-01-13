3d735fc44cdfd61c5b1ff64d41f9633e2e456078
fageer <fageern@gmail.com>
2024-01-13 15:57:35 +0300

    0. All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,18 +8,16 @@ conn = MySQLdb.connect(host='localhost',
                        passwd=argv[2], 
                        database=argv[3])
 
+
 cur = conn.cursor()
 
 
-cur.execute("""SELECT GROUP_CONCAT(name ORDER BY id ASC SEPARATOR ', ')
+cur.execute("""SELECT name
                 FROM cities
                 JOIN states ON cities.state_id = states.id
                 WHERE states.name = %s
+                ORDER BY id ASC
                 """, (argv[4], ))
 
-records = cur.fetchall()
 
-if records:
-    print(records)
-else:
-    print("")
+print(", ".join(map(lambda x: x[0], cur.fetchall())))
