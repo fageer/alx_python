ea1f5a823b84e5acaa53df3e699a52a880a6afb3
fageer <fageern@gmail.com>
2024-01-20 17:15:37 +0300

    2. C is fun!

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -8,14 +8,15 @@ def display ():
     return "Hello HBNB!"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_c ():
-    return "C {}".format(text.replace("_", " "))
-
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb ():
     return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c (text):
+    return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == '__main__':
     app.run(debug=True, host= "0.0.0.0")
