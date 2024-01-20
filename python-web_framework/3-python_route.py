66980166d86ce4ced2f8e09a23d1f3b12171a91b
fageer <fageern@gmail.com>
2024-01-20 18:56:58 +0300

    3. Python is cool!

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -20,7 +20,7 @@ def display_c (text):
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/<text>', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python (text):
     return "Python {}".format(text.replace("_", " "))
