26c75da3d772a339e7cdd5b20688d97f76ad0994
fageer <fageern@gmail.com>
2024-01-21 01:16:29 +0300

    6. Odd or even?

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -36,7 +36,7 @@ def display_nInt (n):
     return render_template("5-number.html", number = n)
 
 
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def display_nOddOrEven (n):
     return render_template("6-number_odd_or_even.html", number = n)
 
