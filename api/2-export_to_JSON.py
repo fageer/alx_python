9977d8eb8cd15a1beaf04681ddc6b392fee914b5
fageer <fageern@gmail.com>
2024-02-24 15:59:40 +0300

    2. Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -13,11 +13,17 @@ def getData(id):
     todourl = "{}/todos".format(usersurl)
 
     request1 = requests.get(usersurl)
+    if request1.status_code != 200:
+        print(f"Employee with ID {id} not found.")
+        return
     results = request1.json()
     userid = results['id']
     username = results['username']
 
     request2 = requests.get(todourl)
+    if request2.status_code != 200:
+        print(f"Unable to fetch TODO list for employee with ID {id}.")
+        return
     tasks = request2.json()
 
     alldata = {}
