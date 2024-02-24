bfc4bdc73c7eb6b09e09634ad56396c4277ee9b5
fageer <fageern@gmail.com>
2024-02-24 15:26:20 +0300

    2. Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -13,27 +13,26 @@ def getData(id):
     todourl = "{}/todos".format(usersurl)
 
     request1 = requests.get(usersurl)
-    result = request1.json()
-    userid = result['id']
-    username = result['username']
+    results = request1.json()
+    userid = results['id']
+    username = results['username']
 
     request2 = requests.get(todourl)
     tasks = request2.json()
-
-    jsondata = {
-        str(userid): [
+    alldata = {}
+    jsondata = [
             {"task": task['title'], "completed": task['completed'], "username": username}
             for task in tasks
         ]
-    }
+    alldata[str(userid)] = jsondata
 
     with open("{}.json".format(userid), "w") as jsonfile:
-        json.dump(jsondata, jsonfile)
+        json.dump(alldata, jsonfile)
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        id = sys.argv[1]
+        id = int(sys.argv[1])
     else:
         id = 1
     getData(id)
